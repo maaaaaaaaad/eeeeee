d9877fb7915db56c8fa1382c11f481cdb8da27f9
maaaaaaaaad <integral5137@gmail.com>
2026-02-04 12:28:33 +0900

    add: routes for login, sign-up, and home pages

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_owner/config/theme.dart';
+import 'package:mobile_owner/features/auth/presentation/pages/login_page.dart';
+import 'package:mobile_owner/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:mobile_owner/features/auth/presentation/pages/splash_page.dart';
+import 'package:mobile_owner/features/home/presentation/pages/home_page.dart';
 
 class OwnerApp extends StatelessWidget {
   const OwnerApp({super.key});
@@ -11,11 +15,12 @@ class OwnerApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: AppTheme.lightTheme,
-      home: const Scaffold(
-        body: Center(
-          child: Text('젤로마크 사장님 앱'),
-        ),
-      ),
+      home: const SplashPage(),
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/sign-up': (context) => const SignUpPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
