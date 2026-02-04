a6719bd6a1a806d114242d6a3d2d33c45c516b31
maaaaaaaaad <integral5137@gmail.com>
2026-02-04 11:59:45 +0900

    add: AuthToken entity

---
+++ lib/features/auth/domain/entities/auth_token.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class AuthToken extends Equatable {
+  final String accessToken;
+  final String refreshToken;
+
+  const AuthToken({
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  @override
+  List<Object?> get props => [accessToken, refreshToken];
+}
