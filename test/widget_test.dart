5308892bd2712a8d13cf676b6e25f46528242f28
maaaaaaaaad <integral5137@gmail.com>
2026-02-04 12:28:41 +0900

    refactor: widget test for LoginPage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mobile_owner/app.dart';
+import 'package:mobile_owner/features/auth/presentation/pages/login_page.dart';
 
 void main() {
-  testWidgets('OwnerApp renders correctly', (WidgetTester tester) async {
+  testWidgets('LoginPage renders correctly', (WidgetTester tester) async {
     await tester.pumpWidget(
-      const ProviderScope(child: OwnerApp()),
+      const ProviderScope(
+        child: MaterialApp(
+          home: LoginPage(),
+        ),
+      ),
     );
 
-    expect(find.text('젤로마크 사장님 앱'), findsOneWidget);
+    expect(find.text('젤로마크 사장님'), findsOneWidget);
+    expect(find.text('사장님 전용 앱에 로그인하세요'), findsOneWidget);
+    expect(find.text('이메일'), findsOneWidget);
+    expect(find.text('비밀번호'), findsOneWidget);
+    expect(find.text('로그인'), findsOneWidget);
+    expect(find.text('회원가입'), findsOneWidget);
   });
 }
