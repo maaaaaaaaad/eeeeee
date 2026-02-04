ad7c057ef92e918587cd670b93ac313b39c0b0ab
maaaaaaaaad <integral5137@gmail.com>
2026-02-04 11:41:47 +0900

    add: app color palette

---
+++ lib/shared/theme/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6B4EFF);
+  static const Color primaryLight = Color(0xFF9D8AFF);
+  static const Color primaryDark = Color(0xFF4930D9);
+
+  static const Color secondary = Color(0xFF03DAC6);
+  static const Color secondaryLight = Color(0xFF66FFF8);
+  static const Color secondaryDark = Color(0xFF00A896);
+
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color error = Color(0xFFB00020);
+
+  static const Color textPrimary = Color(0xFF1A1A1A);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color textHint = Color(0xFFBDBDBD);
+
+  static const Color divider = Color(0xFFE0E0E0);
+  static const Color disabled = Color(0xFFBDBDBD);
+}
