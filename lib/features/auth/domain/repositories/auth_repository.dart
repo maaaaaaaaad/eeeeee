f4bcd3240c646abeffbe0449a4a6ca9c79c859d2
maaaaaaaaad <integral5137@gmail.com>
2026-02-04 12:28:01 +0900

    add: signUp method to AuthRepository

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -5,6 +5,13 @@ import 'package:mobile_owner/features/auth/domain/entities/owner.dart';
 
 abstract class AuthRepository {
   Future<Either<Failure, AuthToken>> login(String email, String password);
+  Future<Either<Failure, AuthToken>> signUp({
+    required String email,
+    required String password,
+    required String businessNumber,
+    required String phoneNumber,
+    required String nickname,
+  });
   Future<Either<Failure, AuthToken>> refreshToken(String refreshToken);
   Future<Either<Failure, void>> logout();
   Future<Either<Failure, Owner>> getCurrentOwner();
