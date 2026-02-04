faa191358f97724863caa8e3d62825a41b3c73d5
maaaaaaaaad <integral5137@gmail.com>
2026-02-04 11:59:51 +0900

    add: LogoutUseCase

---
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:mobile_owner/core/error/failure.dart';
+import 'package:mobile_owner/core/usecase/usecase.dart';
+import 'package:mobile_owner/features/auth/domain/repositories/auth_repository.dart';
+
+class LogoutUseCase extends UseCase<Either<Failure, void>, NoParams> {
+  final AuthRepository repository;
+
+  LogoutUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, void>> call(NoParams params) {
+    return repository.logout();
+  }
+}
