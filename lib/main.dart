dc5137dbc334c5d9fbf99356f193acfc789634b3
maaaaaaaaad <integral5137@gmail.com>
2026-02-07 20:50:59 +0900

    refactor: main with SharedPreferences init

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_owner/app.dart';
 import 'package:mobile_owner/config/app_config.dart';
+import 'package:mobile_owner/features/onboarding/data/datasources/onboarding_local_datasource.dart';
+import 'package:mobile_owner/features/onboarding/presentation/providers/onboarding_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppConfig.initializeApp();
-  runApp(const ProviderScope(child: OwnerApp()));
+
+  final prefs = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        onboardingLocalDataSourceProvider.overrideWithValue(
+          OnboardingLocalDataSourceImpl(prefs: prefs),
+        ),
+      ],
+      child: const OwnerApp(),
+    ),
+  );
 }
