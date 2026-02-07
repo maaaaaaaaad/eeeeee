3484d4f7da9ff538e4d360dee0aa14f7db15a95c
maaaaaaaaad <integral5137@gmail.com>
2026-02-08 01:01:14 +0900

    add: create_treatment_request

---
+++ lib/features/treatment/data/models/create_treatment_request.dart
@@ -0,0 +1,22 @@
+class CreateTreatmentRequest {
+  final String name;
+  final int price;
+  final int duration;
+  final String? description;
+
+  const CreateTreatmentRequest({
+    required this.name,
+    required this.price,
+    required this.duration,
+    this.description,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'price': price,
+      'duration': duration,
+      'description': description,
+    };
+  }
+}
