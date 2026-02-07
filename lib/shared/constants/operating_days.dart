be7c5579ea076a524b9b57863c4cdda7310df954
maaaaaaaaad <integral5137@gmail.com>
2026-02-07 20:49:06 +0900

    add: operating_days constants

---
+++ lib/shared/constants/operating_days.dart
@@ -0,0 +1,23 @@
+class OperatingDays {
+  OperatingDays._();
+
+  static const List<String> orderedKeys = [
+    'MONDAY',
+    'TUESDAY',
+    'WEDNESDAY',
+    'THURSDAY',
+    'FRIDAY',
+    'SATURDAY',
+    'SUNDAY',
+  ];
+
+  static const Map<String, String> toKorean = {
+    'MONDAY': '월',
+    'TUESDAY': '화',
+    'WEDNESDAY': '수',
+    'THURSDAY': '목',
+    'FRIDAY': '금',
+    'SATURDAY': '토',
+    'SUNDAY': '일',
+  };
+}
