f2d934de99ec719630896f5b550b88f921cae59f
maaaaaaaaad <integral5137@gmail.com>
2026-02-07 20:49:53 +0900

    add: create_beautishop_request_test

---
+++ test/features/beautishop/data/models/create_beautishop_request_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile_owner/features/beautishop/data/models/create_beautishop_request.dart';
+
+void main() {
+  group('CreateBeautishopRequest', () {
+    test('should convert to JSON with all fields', () {
+      const request = CreateBeautishopRequest(
+        name: '테스트 샵',
+        regNum: '1234567890',
+        phoneNumber: '01012345678',
+        address: '서울시 강남구',
+        latitude: 37.5665,
+        longitude: 126.978,
+        operatingTime: {'MONDAY': '09:00-18:00'},
+        shopDescription: '테스트 설명',
+        shopImages: ['https://example.com/img.jpg'],
+      );
+
+      final json = request.toJson();
+
+      expect(json['name'], '테스트 샵');
+      expect(json['regNum'], '1234567890');
+      expect(json['phoneNumber'], '01012345678');
+      expect(json['address'], '서울시 강남구');
+      expect(json['latitude'], 37.5665);
+      expect(json['longitude'], 126.978);
+      expect(json['operatingTime'], {'MONDAY': '09:00-18:00'});
+      expect(json['shopDescription'], '테스트 설명');
+      expect(json['shopImages'], ['https://example.com/img.jpg']);
+    });
+
+    test('should handle null description in JSON', () {
+      const request = CreateBeautishopRequest(
+        name: '테스트 샵',
+        regNum: '1234567890',
+        phoneNumber: '01012345678',
+        address: '서울시 강남구',
+        latitude: 37.5665,
+        longitude: 126.978,
+        operatingTime: {},
+        shopImages: [],
+      );
+
+      final json = request.toJson();
+
+      expect(json['shopDescription'], isNull);
+      expect(json['shopImages'], isEmpty);
+    });
+  });
+}
