a1d885491f0d0a21012f8200ced0623b8b12e288
maaaaaaaaad <integral5137@gmail.com>
2026-02-10 14:32:52 +0900

    add: FCM configuration to app config

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mobile_owner/core/notification/fcm_service.dart';
 
 class AppConfig {
   static Future<void> initializeApp() async {
@@ -15,5 +18,8 @@ class AppConfig {
         statusBarBrightness: Brightness.light,
       ),
     );
+
+    await Firebase.initializeApp();
+    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   }
 }
