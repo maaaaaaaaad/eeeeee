e7133120f2a18f71a51fc632029dd518aa878896
maaaaaaaaad <integral5137@gmail.com>
2026-02-12 17:59:25 +0900

    add: RatingStarsDisplay widget

---
+++ lib/features/review/presentation/widgets/rating_stars_display.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class RatingStarsDisplay extends StatelessWidget {
+  final int rating;
+  final double size;
+
+  const RatingStarsDisplay({
+    super.key,
+    required this.rating,
+    this.size = 16,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return Icon(
+          index < rating ? Icons.star : Icons.star_border,
+          color: Colors.amber,
+          size: size,
+        );
+      }),
+    );
+  }
+}
