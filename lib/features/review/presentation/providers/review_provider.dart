cf0b00465281be29afdb5c8333289c0257ff26d7
maaaaaaaaad <integral5137@gmail.com>
2026-02-13 00:58:01 +0900

    add: reply and delete usecase providers

--- lib/features/review/presentation/providers/review_provider.dart
+++ lib/features/review/presentation/providers/review_provider.dart
@@ -3,7 +3,9 @@ import 'package:mobile_owner/features/auth/presentation/providers/auth_provider.
 import 'package:mobile_owner/features/review/data/datasources/review_remote_datasource.dart';
 import 'package:mobile_owner/features/review/data/repositories/review_repository_impl.dart';
 import 'package:mobile_owner/features/review/domain/repositories/review_repository.dart';
+import 'package:mobile_owner/features/review/domain/usecases/delete_review_reply_usecase.dart';
 import 'package:mobile_owner/features/review/domain/usecases/get_shop_reviews_usecase.dart';
+import 'package:mobile_owner/features/review/domain/usecases/reply_to_review_usecase.dart';
 
 final reviewRemoteDataSourceProvider =
     Provider<ReviewRemoteDataSource>((ref) {
@@ -19,4 +21,14 @@ final reviewRepositoryProvider = Provider<ReviewRepository>((ref) {
 final getShopReviewsUseCaseProvider =
     Provider<GetShopReviewsUseCase>((ref) {
   return GetShopReviewsUseCase(ref.watch(reviewRepositoryProvider));
+});
+
+final replyToReviewUseCaseProvider =
+    Provider<ReplyToReviewUseCase>((ref) {
+  return ReplyToReviewUseCase(ref.watch(reviewRepositoryProvider));
+});
+
+final deleteReviewReplyUseCaseProvider =
+    Provider<DeleteReviewReplyUseCase>((ref) {
+  return DeleteReviewReplyUseCase(ref.watch(reviewRepositoryProvider));
 });
