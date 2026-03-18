c2c5c8bb3ddf890be5cd7efd63b888b4bd66afad
maaaaaaaaad <integral5137@gmail.com>
2026-03-19 02:03:39 +0900

    add: ReservationRepository에 getOwnerReservations 추가

--- lib/features/reservation/domain/repositories/reservation_repository.dart
+++ lib/features/reservation/domain/repositories/reservation_repository.dart
@@ -5,6 +5,7 @@ import 'package:mobile_owner/features/reservation/domain/entities/reservation.da
 
 abstract class ReservationRepository {
   Future<Either<Failure, List<Reservation>>> getShopReservations(String shopId);
+  Future<Either<Failure, List<Reservation>>> getOwnerReservations();
   Future<Either<Failure, Reservation>> getReservation(String reservationId);
   Future<Either<Failure, Reservation>> confirmReservation(String reservationId);
   Future<Either<Failure, Reservation>> rejectReservation(
