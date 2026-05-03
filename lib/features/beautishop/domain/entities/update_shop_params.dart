be39aba74493f4cf25e72fc75539b984fb32b597
maaaaaaaaad <integral5137@gmail.com>
2026-05-03 17:23:26 +0900

    add: UpdateShopParams에 menuImages 추가

--- lib/features/beautishop/domain/entities/update_shop_params.dart
+++ lib/features/beautishop/domain/entities/update_shop_params.dart
@@ -5,14 +5,17 @@ class UpdateShopParams extends Equatable {
   final Map<String, String> operatingTime;
   final String? shopDescription;
   final List<String> shopImages;
+  final List<String> menuImages;
 
   const UpdateShopParams({
     required this.shopId,
     required this.operatingTime,
     this.shopDescription,
     required this.shopImages,
+    this.menuImages = const [],
   });
 
   @override
-  List<Object?> get props => [shopId, operatingTime, shopDescription, shopImages];
+  List<Object?> get props =>
+      [shopId, operatingTime, shopDescription, shopImages, menuImages];
 }
