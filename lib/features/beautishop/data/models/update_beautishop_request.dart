5c5174c6685cd34fd8cef1423cc85c58ed26952f
maaaaaaaaad <integral5137@gmail.com>
2026-05-03 17:23:26 +0900

    add: UpdateBeautishopRequest 모델에 menuImages 추가

--- lib/features/beautishop/data/models/update_beautishop_request.dart
+++ lib/features/beautishop/data/models/update_beautishop_request.dart
@@ -2,11 +2,13 @@ class UpdateBeautishopRequest {
   final Map<String, String> operatingTime;
   final String? shopDescription;
   final List<String> shopImages;
+  final List<String> menuImages;
 
   const UpdateBeautishopRequest({
     required this.operatingTime,
     this.shopDescription,
     required this.shopImages,
+    this.menuImages = const [],
   });
 
   Map<String, dynamic> toJson() {
@@ -14,6 +16,7 @@ class UpdateBeautishopRequest {
       'operatingTime': operatingTime,
       'shopDescription': shopDescription,
       'shopImages': shopImages,
+      'menuImages': menuImages,
     };
   }
 }
