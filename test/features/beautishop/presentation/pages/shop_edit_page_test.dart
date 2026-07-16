6791816f2bbe214e26ff166ee9cf8138fb294bd4
maaaaaaaaad <integral5137@gmail.com>
2026-07-16 09:17:45 +0900

    refactor: shop_edit_page_test에 listDesignersUseCase mock override 추가

--- test/features/beautishop/presentation/pages/shop_edit_page_test.dart
+++ test/features/beautishop/presentation/pages/shop_edit_page_test.dart
@@ -1,9 +1,17 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:mobile_owner/core/error/failure.dart';
 import 'package:mobile_owner/features/beautishop/presentation/pages/shop_edit_page.dart';
 import 'package:mobile_owner/features/beautishop/presentation/widgets/shop_image_picker.dart';
+import 'package:mobile_owner/features/designer/domain/entities/designer.dart';
+import 'package:mobile_owner/features/designer/domain/usecases/list_designers_usecase.dart';
+import 'package:mobile_owner/features/designer/presentation/providers/designer_provider.dart';
 import 'package:mobile_owner/features/home/domain/entities/beauty_shop.dart';
+
+class _MockListDesignersUseCase extends Mock implements ListDesignersUseCase {}
 
 BeautyShop _shop() => const BeautyShop(
   id: '1',
@@ -22,6 +30,14 @@ BeautyShop _shop() => const BeautyShop(
 );
 
 void main() {
+  late _MockListDesignersUseCase mockListDesigners;
+
+  setUp(() {
+    mockListDesigners = _MockListDesignersUseCase();
+    when(() => mockListDesigners(any())).thenAnswer(
+        (_) async => const Right<Failure, List<Designer>>(<Designer>[]));
+  });
+
   Future<void> pumpPage(WidgetTester tester) async {
     tester.view.physicalSize = const Size(1200, 4000);
     tester.view.devicePixelRatio = 1.0;
@@ -30,6 +46,9 @@ void main() {
 
     await tester.pumpWidget(
       ProviderScope(
+        overrides: [
+          listDesignersUseCaseProvider.overrideWithValue(mockListDesigners),
+        ],
         child: MaterialApp(home: ShopEditPage(shop: _shop())),
       ),
     );
